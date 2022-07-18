c38f61ae40369d694580b9bff9ab95ad6ab30af6
abhishek yadav <absydv@gmail.com>
2022-07-19 01:36:52 +0200

    updating and adding interface for type check

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -7,6 +7,8 @@ export interface Video {
   id: number;
   catIds: number[];
   name: string;
+  formats: Formats;
+  releaseDate: string;
 }
 
 export interface Author {
@@ -19,5 +21,19 @@ export interface ProcessedVideo {
   id: number;
   name: string;
   author: string;
-  categories: string[];
+  categories: Category[]; // using category object because: better to have value(id) and label together
+  authorId: number;  // using authorId to get author indentification.
+  formats: Formats;
+  releaseDate: string;
+}
+
+
+export interface Formats {
+  [key: string]: FormatData
+}
+
+export interface FormatData {
+  res: string, 
+  size: number,
+  name?: string, // name:optional because: it will be only used once change the Formats from onbject to  array for sorting 
 }
