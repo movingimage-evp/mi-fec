31957ac337aa66b50576efd4329187b24150e8e7
Robert Kirsz <robert.kirsz@gmail.com>
2022-08-29 14:00:03 +0200

    Add simple layout; refactor code

--- src/components/videos-table.tsx
+++ src/components/videos-table.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { ProcessedVideo } from '../common/interfaces';
+import type { ProcessedVideo } from '../common/interfaces';
 import styles from './videos-table.module.css';
 
-interface VideosTableProps {
+type VideosTableProps = {
   videos: ProcessedVideo[];
-}
-
-export const VideosTable: React.FC<VideosTableProps> = ({ videos }) => {
-  return (
-    <div className={styles.wrapper}>
-      <table className={styles.videosTable}>
-        <thead>
-          <tr>
-            <th>Video Name</th>
-            <th>Author</th>
-            <th>Categories</th>
-            <th>Options</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {videos.map((video) => (
-            <tr key={video.id}>
-              <td>{video.name}</td>
-              <td>{video.author}</td>
-              <td>{video.categories.join(', ')}</td>
-              <td> {/* add buttons here as needed */} </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
 };
+
+export const VideosTable = ({ videos }: VideosTableProps) => (
+  <div className={styles.wrapper}>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>Video Name</th>
+          <th>Author</th>
+          <th>Categories</th>
+          <th>Options</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {videos.map((video) => (
+          <tr key={video.id}>
+            <td>{video.name}</td>
+            <td>{video.author}</td>
+            <td>{video.categories.join(', ')}</td>
+            <td>
+              <button>Edit</button>
+              <button>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
