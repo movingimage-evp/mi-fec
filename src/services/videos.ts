7172be38dabf0b8360ec9a4d6216d94dae533b04
Maria Schmidt <106662885+MariaMi0106@users.noreply.github.com>
2026-08-18 23:23:26 +0200

    FE-77 | Prepare app for dependency agent test (#50)

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -5,5 +5,14 @@ import { ProcessedVideo } from '../common/interfaces';
 export const getVideos = async (): Promise<ProcessedVideo[]> => {
   const [categories, authors] = await Promise.all([getCategories(), getAuthors()]);
 
-  return []; // TODO: implement
+  const categoryNamesById = new Map(categories.map(({ id, name }) => [id, name]));
+
+  return authors.flatMap((author) =>
+    author.videos.map((video) => ({
+      id: video.id,
+      name: video.name,
+      author: author.name,
+      categories: video.catIds.map((catId) => categoryNamesById.get(catId)).filter((name): name is string => name !== undefined),
+    }))
+  );
 };
